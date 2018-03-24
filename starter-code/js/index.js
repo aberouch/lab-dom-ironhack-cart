c62cd10cf51326458139b95bc8b98250d8fabae5
thebinaryfelix <mateus.cfoliveira@gmail.com>
2018-03-24 20:37:42 +0100

    Adding containers with js

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -2,8 +2,30 @@ function deleteItem(e){
 
 }
 
-function getPriceByProduct(itemNode){
+function getPriceByProduct(){
+  console.log("Total price clicked!");
+  var price = "";
+  var quantity = 0;
+  var totalPrice = 0;
+  var end = 1;
+  var i = 1;
 
+  while(end != 0){
+
+    price = document.getElementById("priceValue-"+i).textContent;
+
+    quantity = document.getElementById("quantity-"+i).value;
+    totalPrice = parseFloat(price) * quantity;
+
+    document.getElementById('totalPrice-'+i).innerHTML = '$' + totalPrice;
+    
+    console.log("Price is: " + price + " and qty is: " + quantity);
+    
+    if(price == null || quantity == null){
+      end = 0;
+    }
+    i++;
+  }
 }
 
 function updatePriceByProduct(productPrice, index){
@@ -11,28 +33,8 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
-    console.log("Total price clicked!");
-    var price = "";
-    var quantity = 0;
-    var totalPrice = 0;
-    var end = 1;
-    var i = 1;
 
-    while(end != 0){
-      
-      price = document.getElementById("priceValue-"+i).textContent;
-      quantity = document.getElementById("quantity-"+i).value;
-      totalPrice = parseFloat(price) * quantity;
-      
-      document.getElementById('totalPrice-'+i).innerHTML = '$' + totalPrice;
-      
-      console.log("Price is: " + price + " and qty is: " + quantity);
-      
-      if(price == null || quantity == null){
-        end = 1;
-      }
-      i++;
-    }
+
 }
 
 function createQuantityInput(){
@@ -55,29 +57,56 @@ function createNewItemRow(itemName, itemUnitPrice){
 
 }
 
-
-/* function createNewItem(){
-
-} */
-
 function createNewItem(){
 
+  console.log("createNewItem() was called!");
   var main = document.getElementById("main");
-  var newElement = document.createElement("div");
-  
-  newElement.id = "new-product-"+i;
-  i++;
+  console.log("main was found!");
+  var findLastContainer = "";
+  var i=1;
+  var end = 1;
 
+  console.log("i = " + i);
+
+  while(end != 0){
+    var productName = "new-product-"+i;
+
+    console.log("Enter while");
+
+    findLastContainer = document.getElementById(productName);
+
+    console.log("findLastContainer = " + findLastContainer);
+
+    if(findLastContainer === null){
+      end = 0;
+    }else{
+      i++;
+    }
+  }
+
+  console.log("Exit while");
+  
+  var newElement = document.createElement("div");
+
+  console.log("New Element Created: " + newElement);
+  
+  console.log("i = " + i);
+
+  newElement.id = "new-product-"+i;
+
+  console.log("New Element ID: " + newElement.id);
   main.appendChild(newElement);
-/* 
-  var newProduct = document.getElementById("new-product");
+
+  var newProduct = document.getElementById("new-product-"+i);
   newProduct.classList.add("flexbox");
 
-  for(var j=0 ; j<5 ; j++){
+
+
+/*   for(var j=0 ; j<5 ; j++){
     newProduct.appendChild("div");
-  }
+  } */
   
-  var product = document.getElementById("product");
+/*   var product = document.getElementById("product");
   product.appendChild("span");
   var price = document.getElementById("price");
   price.appendChild("span");
@@ -95,8 +124,8 @@ window.onload = function(){
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
   var indexNewElement=0;
-  calculatePriceButton.onclick = getTotalPrice;
-  //createItemButton.onclick = createNewItem;
+  calculatePriceButton.onclick = getPriceByProduct;
+  createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
