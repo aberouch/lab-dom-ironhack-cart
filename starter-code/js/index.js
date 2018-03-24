712ea2fb39286546b395f67c96bd69a47cc8c03a
thebinaryfelix <mateus.cfoliveira@gmail.com>
2018-03-24 17:24:04 +0100

    function to calculate multiple prices

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -12,14 +12,27 @@ function updatePriceByProduct(productPrice, index){
 
 function getTotalPrice() {
     console.log("Total price clicked!");
-    var price = document.getElementById("priceValue").textContent;
-    var quantity = document.getElementById("quantity").value;
-    console.log("Price is: " + price + " and qty is: " + quantity);
+    var price = "";
+    var quantity = 0;
+    var totalPrice = 0;
+    var end = 1;
+    var i = 1;
 
-    var totalPrice = parseFloat(price) * quantity;
-
-    document.getElementById('totalPrice').innerHTML = '$' + totalPrice;
-
+    while(end != 0){
+      
+      price = document.getElementById("priceValue-"+i).textContent;
+      quantity = document.getElementById("quantity-"+i).value;
+      totalPrice = parseFloat(price) * quantity;
+      
+      document.getElementById('totalPrice-'+i).innerHTML = '$' + totalPrice;
+      
+      console.log("Price is: " + price + " and qty is: " + quantity);
+      
+      if(price == null || quantity == null){
+        end = 1;
+      }
+      i++;
+    }
 }
 
 function createQuantityInput(){
@@ -42,7 +55,38 @@ function createNewItemRow(itemName, itemUnitPrice){
 
 }
 
+
+/* function createNewItem(){
+
+} */
+
 function createNewItem(){
+
+  var main = document.getElementById("main");
+  var newElement = document.createElement("div");
+  
+  newElement.id = "new-product-"+i;
+  i++;
+
+  main.appendChild(newElement);
+/* 
+  var newProduct = document.getElementById("new-product");
+  newProduct.classList.add("flexbox");
+
+  for(var j=0 ; j<5 ; j++){
+    newProduct.appendChild("div");
+  }
+  
+  var product = document.getElementById("product");
+  product.appendChild("span");
+  var price = document.getElementById("price");
+  price.appendChild("span");
+  var qty = document.getElementById("qty");
+  qty.appendChild("label");
+  var total = document.getElementById("total");
+  total.appendChild("span");
+  var deleteBtn = document.getElementById("delete");
+  deleteBtn.appendChild("button"); */
 
 }
 
@@ -50,9 +94,9 @@ window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
-
+  var indexNewElement=0;
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  //createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
